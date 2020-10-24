d599909329d82c6a023c0af0f3bfc6ad727698b0
lashatatkhashvili <@users.noreply.github.com>
2020-10-24 19:41:11 +0400

    Updates

---
+++ precache-manifest.586faef8b39cd3bb009f4925b4bf3cc1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37ca3864fd747ccf19c53aabb2c3ff6a",
+    "url": "/Hotell/index.html"
+  },
+  {
+    "revision": "09575d78f634cbfc8d3b",
+    "url": "/Hotell/static/css/main.22544605.chunk.css"
+  },
+  {
+    "revision": "42d4ebf9818816d3be2c",
+    "url": "/Hotell/static/js/2.2afba1bb.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Hotell/static/js/2.2afba1bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09575d78f634cbfc8d3b",
+    "url": "/Hotell/static/js/main.ff80cc5c.chunk.js"
+  },
+  {
+    "revision": "ebab88b99109cacc8c2b",
+    "url": "/Hotell/static/js/runtime-main.b1ad4991.js"
+  },
+  {
+    "revision": "aebaec51b2f82a2993fda821ca04e228",
+    "url": "/Hotell/static/media/defaultBcg.aebaec51.jpeg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/Hotell/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "9f0cccc1e406a5aa9ef6ee36229762c3",
+    "url": "/Hotell/static/media/logo.9f0cccc1.svg"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/Hotell/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "052c76815f889f20b8a498c19a212c50",
+    "url": "/Hotell/static/media/room-2.052c7681.jpeg"
+  }
+]);
